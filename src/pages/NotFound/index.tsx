72e3dc8d1422590f432cb63b50b4fbf77033e783
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-14 14:11:21 +0300

    feat: создает страницу NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,30 @@
+import { Button, Container, Typography } from '@mui/material'
+import { AppRoute } from '@utils/const'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound: React.FC = () => {
-	return <div>Error 404. Страница не найдена</div>
+	return (
+		<Container
+			maxWidth='xl'
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				gap: '30px',
+				padding: '30px',
+			}}>
+			<Typography variant='h4' component='h1' gutterBottom>
+				Ошибка 404
+			</Typography>
+			<Typography variant='subtitle1' component='p' gutterBottom>
+				Страница не найдена.
+			</Typography>
+			<Link to={AppRoute.Main}>
+				<Button variant='contained'>Вернуться на главную страницу</Button>
+			</Link>
+		</Container>
+	)
 }
 
 export default NotFound
