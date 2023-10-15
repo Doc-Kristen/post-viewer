5e606655216a35333bc711a85a4a3805a14a3a17
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-15 16:18:06 +0300

    feat: реализует виртуализацию списка постов

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,23 +1,32 @@
 import { PostItem } from '@components/index'
-import { List, ListSubheader } from '@mui/material'
+import { ListSubheader } from '@mui/material'
 import { TPost } from 'types/index'
 import React from 'react'
+import { FixedSizeList, ListChildComponentProps } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
 
 type PostListProps = {
 	posts: TPost[]
 }
 
-const PostList: React.FC<PostListProps> = ({posts}) => {
+const PostList: React.FC<PostListProps> = ({ posts }) => {
+	const Row = ({ index, style }: ListChildComponentProps) => (
+		<div style={style}>
+			<PostItem key={posts[index].id} post={posts[index]} index={index} />
+		</div>
+	)
 	return (
-		<div>
+		<div style={{height: '80vh' }}>
 			<ListSubheader component='h2' sx={{ fontSize: '30px', margin: '20px' }}>
 				Список постов:
 			</ListSubheader>
-			<List>
-				{posts.map((item, index) => {
-					return <PostItem key={item.id} post={item} index={index} />
-				})}
-			</List>
+			<AutoSizer>
+				{({ height, width }) => (
+					<FixedSizeList height={height} width={width} itemCount={100} itemSize={64}>
+						{Row}
+					</FixedSizeList>
+				)}
+			</AutoSizer>
 		</div>
 	)
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,17 +1,17 @@
 import { PostList } from '@components/index'
 import { Container } from '@mui/material'
-import { useGetPaginatedPostsQuery } from '@services/postApi'
+import { useGetAllPostsQuery } from '@services/postApi'
 import React from 'react'
 
 const Main: React.FC = () => {
-	const { data, isLoading, error } = useGetPaginatedPostsQuery({ startIndex: 0, stopIndex: 10 })
+	const { data, isLoading, error } = useGetAllPostsQuery('')
 
 	if (isLoading) return <div>Загрузка...</div>
 
 	if (error) return <div>Ошибка загрузки</div>
 
 	return (
-		<Container maxWidth='xl'>
+		<Container>
 			<PostList posts={data} />
 		</Container>
 	)
