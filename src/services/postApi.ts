61cb78a480164492228684f61f35e98e0f21e2f4
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-16 18:58:01 +0300

    fix: убирает значения пагинации по умолчанию

--- src/services/postApi.ts
+++ src/services/postApi.ts
@@ -13,10 +13,12 @@ export const postsApi = createApi({
 			query: id => `/posts/${id}`,
 		}),
 		getPaginatedPosts: builder.query({
-			query: ({ startIndex = 0, stopIndex = 15 }) =>
-				`/posts?_start=${startIndex}&_end=${stopIndex}`,
+			query: ({ startIndex, stopIndex }) => `/posts?_start=${startIndex}&_end=${stopIndex}`,
 			transformResponse(baseQueryReturnValue, meta) {
-				return { baseQueryReturnValue, totalCount: Number(meta?.response?.headers.get('X-Total-Count')) }
+				return {
+					baseQueryReturnValue,
+					totalCount: Number(meta?.response?.headers.get('X-Total-Count')),
+				}
 			},
 		}),
 	}),
