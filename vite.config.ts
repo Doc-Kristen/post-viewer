daa356d2273100ac1bed0e6bf99241c6ab042074
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-26 02:45:47 +0300

    optimize: оптимизирует сборку билда

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), compression()],
 	resolve: {
 		alias: {
 			'@components': '/src/components',
