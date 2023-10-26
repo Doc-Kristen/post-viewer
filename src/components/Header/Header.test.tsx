8f15a97dc8a96cc8cb4128ccf634a74e130556a8
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-26 21:12:47 +0300

    test: создает тест для компонента Header

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom' 
+import Header from './index'
+
+describe('Component: Header', () => {
+	it('should render correctly', () => {
+		render(
+			<Router>
+				<Header />
+			</Router>,
+		)
+		const title = screen.queryByText(/PostViewer/i)
+		expect(title).toBeVisible()
+	})
+})
