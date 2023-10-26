1accb85a8f3d688ad7c54db52d0ce7270939b37e
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-26 10:50:08 +0300

    feat: меняет тему оформления

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,28 @@ import { Route, Routes } from 'react-router-dom'
 import { MainLayout } from '@layouts/index'
 import { AppRoute } from '@utils/const'
 import { Main, NotFound, Post } from '@pages/index'
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
+import createTheme from '@mui/material/styles/createTheme'
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#3F51B5',
+		},
+	},
+})
 
 const App: React.FC = () => {
 	return (
-		<Routes>
-			<Route path={AppRoute.Main} element={<MainLayout />}>
-				<Route path="" element={<Main />} />
-				<Route path={AppRoute.Post} element={<Post />} />
-				<Route path={AppRoute.NotFound} element={<NotFound />} />
-			</Route>
-		</Routes>
+		<ThemeProvider theme={theme}>
+			<Routes>
+				<Route path={AppRoute.Main} element={<MainLayout />}>
+					<Route path='' element={<Main />} />
+					<Route path={AppRoute.Post} element={<Post />} />
+					<Route path={AppRoute.NotFound} element={<NotFound />} />
+				</Route>
+			</Routes>
+		</ThemeProvider>
 	)
 }
 
