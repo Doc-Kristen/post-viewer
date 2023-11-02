02b217e84df7f97395771e5cecfd484ecb408072
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-11-02 15:03:08 +0300

    service: добавляет настройки для тест-сервера

--- src/test/serverSetup.tsx
+++ src/test/serverSetup.tsx
@@ -21,3 +21,14 @@ export const errorHandler = [
 ]
 
 export const server = setupServer()
+
+export const configureServerForTesting = () => {
+	// Enable the API mocking before tests.
+	beforeAll(() => server.listen())
+
+	// Reset any runtime request handlers we may add during the tests.
+	afterEach(() => server.resetHandlers())
+
+	// Disable the API mocking after the tests finished.
+	afterAll(() => server.close())
+}
